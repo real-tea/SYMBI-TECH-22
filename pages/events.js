9628610c215b36914bfa2066402486ca259d0eb9
Siddharth Khatri <siddxharth@gmail.com>
2022-09-08 00:20:42 +0530

    Change Font

--- pages/events.js
+++ pages/events.js
@@ -2,26 +2,26 @@ import dynamic from "next/dynamic";
 import Head from "next/head";
 import Event from "../components/Events/Event";
 
-const AnimatedCursor = dynamic(()=>import('react-animated-cursor'),{
-    ssr:false
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+    ssr: false
 });
 
-export default function Events(){
-    return(
+export default function Events() {
+    return (
         <div className="body">
-        <AnimatedCursor 
-        innerSize={15}
-        outerSize={40}
-        color='255, 255, 255'
-        outerAlpha={0.2}
-        innerScale={0.6}
-        outerScale={2}
-        trailingSpeed={7}
-        />
-        <Head>
-            <title>Events @ SymbITech</title>
-        </Head>
-        <Event/>
+            <AnimatedCursor
+                innerSize={15}
+                outerSize={40}
+                color='255, 255, 255'
+                outerAlpha={0.2}
+                innerScale={0.6}
+                outerScale={2}
+                trailingSpeed={7}
+            />
+            <Head>
+                <title>Events @ SymbITech</title>
+            </Head>
+            <Event />
         </div>
     )
 }
