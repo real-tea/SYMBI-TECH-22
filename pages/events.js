b39a53668cc64f245a0de4a5de5fad5574f54960
real-tea <akash.0711a@gmail.com>
2022-09-08 00:04:15 +0530

    events page

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,5 @@
+import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const AnimatedCursor = dynamic(()=>import('react-animated-cursor'),{
     ssr:false
@@ -6,10 +8,18 @@ const AnimatedCursor = dynamic(()=>import('react-animated-cursor'),{
 export default function Events(){
     return(
         <div className="body">
-        <AnimatedCursor
-            innerSize={15}
-            outter
+        <AnimatedCursor 
+        innerSize={15}
+        outerSize={40}
+        color='255, 255, 255'
+        outerAlpha={0.2}
+        innerScale={0.6}
+        outerScale={2}
+        trailingSpeed={7}
         />
+        <Head>
+            <title>Events @ SymbITech</title>
+        </Head>
 
         </div>
     )
