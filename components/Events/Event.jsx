7fa5c2e2446b7996cdc7f24e9f59d60519f88c92
real-tea <akash.0711a@gmail.com>
2022-09-08 01:07:34 +0530

    events pages

--- components/Events/Event.jsx
+++ components/Events/Event.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Container } from "@mui/material";
 
 const Event = () => {
   return (
-    <div></div>
+    <div className="about" id="about">
+      <h1 className="about-header">
+        Events
+      </h1>
+
+      <Container className='abt-cont'>
+        <ul className="about-main-text">
+          {/* <li>Cosmos Game</li> */}
+          <li>Best Out of Good</li>
+          <li>Car Parts Assembly</li>
+          <li>Laser Tag</li>
+          <li>Paint Your galaxy</li>
+          <li>Laser Cutting</li>
+          <li>AI model</li>
+          <li>3D model design</li>
+          <li>Finish and level up</li>
+          <li>TECH MUN</li>
+          <li>Drive the bot</li>
+          <li>Weaving the web</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Container>
+    </div>
   )
 }
 
