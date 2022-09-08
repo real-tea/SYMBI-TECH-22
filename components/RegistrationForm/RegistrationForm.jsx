b0ef23f8c356d8fd02eae05b359e976298ef54c0
Siddharth Khatri <siddxharth@gmail.com>
2022-09-08 10:53:19 +0530

    Add Registraion form

--- components/RegistrationForm/RegistrationForm.jsx
+++ components/RegistrationForm/RegistrationForm.jsx
@@ -1,87 +1,97 @@
 import React from 'react'
 import { Formik } from 'formik';
+import { Input, Button, Row, Col, Grid, Spacer, Textarea, Tooltip } from '@nextui-org/react';
+import Fade from 'react-reveal/Fade';
+import Navbar from '../Navbar/Navbar';
 const RegistrationForm = () => {
     return (
-        <Formik
-            initialValues={{ student_name: '', prn: '', email: '', event_name: '' }}
-            onSubmit={(values, { resetForm, setSubmitting }) => {
-                const data = {
-                    student_name: values.student_name,
-                    prn: values.prn,
-                    email: values.email,
-                    event_name: values.event_name
-                };
-                data.submittedAt = getTimeString();
-                sendData(data, setSubmitting);
-                setModalOpen(true);
-                resetForm({ values: '' });
-            }}
-        // validationSchema={basicSchema}
-        >
-            {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-            }) => (
-                <form className='form-container' onSubmit={handleSubmit} autoComplete="off">
-                    {/* Student Name */}
-                    <input
-                        type="text"
-                        name="student_name"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.student_name}
-                        placeholder="Student Name"
-                    />
-                    <p className="error-msg">{errors.student_name && touched.student_name && errors.student_name}</p>
-                    {/* Email */}
-                    <input
-                        type="email"
-                        name="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        placeholder="Email"
-                    />
-                    <p className="error-msg">{errors.email && touched.email && errors.email}</p>
-                    {/* PRN */}
-                    <input
-                        type="number"
-                        name="prn"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.prn}
-                        placeholder="PRN"
-                    />
-                    <p className="error-msg">{errors.prn && touched.prn && errors.prn}</p>
-                    {/* Event Name */}
-                    <select
-                        type="text"
-                        name="event_name"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.event_name}
-                        placeholder="Select Event"
-                    >
-                        <option value=''>Select Batch</option>
-                        <option value='2019-23'>Event 1</option>
-                        <option value='2020-24'>Event 2</option>
-                        <option value='2021-25'>Event 3</option>
-                        <option value='2022-26'>Event 4</option>
-                        <option value='STAFF'>Test</option>
-                    </select>
-                    <p className="error-msg">{errors.event_name && touched.event_name && errors.event_name}</p>
-                    <button className="submit-btn" type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </form>
-            )}
-        </Formik>
+        <div>
+            <Navbar />
+            <Fade duration={2000} delay={300} small>
+                <Formik
+                    className='formik-form'
+                    initialValues={{ student_name: '', prn: '', email: '', event_name: '' }}
+                    onSubmit={(values, { resetForm, setSubmitting }) => {
+                        const data = {
+                            student_name: values.student_name,
+                            prn: values.prn,
+                            email: values.email,
+                            event_name: values.event_name
+                        };
+                        data.submittedAt = getTimeString();
+                        sendData(data, setSubmitting);
+                        setModalOpen(true);
+                        resetForm({ values: '' });
+                    }}
+                // validationSchema={basicSchema}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <form className='form-container' onSubmit={handleSubmit} autoComplete="off">
+                            {/* Student Name */}
+                            <input
+                                type="text"
+                                name="student_name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.student_name}
+                                placeholder="Student Name"
+                                className='input-field'
+                            />
+                            <p className="error-msg">{errors.student_name && touched.student_name && errors.student_name}</p>
+                            {/* Email */}
+                            <input
+                                type="email"
+                                name="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                                placeholder="Email"
+                            />
+                            <p className="error-msg">{errors.email && touched.email && errors.email}</p>
+                            {/* PRN */}
+                            <input
+                                type="number"
+                                name="prn"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.prn}
+                                placeholder="PRN"
+                            />
+                            <p className="error-msg">{errors.prn && touched.prn && errors.prn}</p>
+                            {/* Event Name */}
+                            <select
+                                type="text"
+                                name="event_name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.event_name}
+                                placeholder="Select Event"
+                            >
+                                <option className='option' value=''>Select Batch</option>
+                                <option className='option' value='2019-23'>Event 1</option>
+                                <option className='option' value='2020-24'>Event 2</option>
+                                <option className='option' value='2021-25'>Event 3</option>
+                                <option className='option' value='2022-26'>Event 4</option>
+                                <option className='option' value='STAFF'>Test</option>
+                            </select>
+                            <p className="error-msg">{errors.event_name && touched.event_name && errors.event_name}</p>
+                            <button className="submit-btn" type="submit" disabled={isSubmitting}>
+                                Submit
+                            </button>
+                        </form>
+                    )}
+                </Formik>
+            </Fade>
+        </div>
     )
 }
 
-export default RegistrationForm
+export default RegistrationForm;
--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import '../components/Projects/Projects.css'
 import '../components/Footer/Footer.css'
 import '../components/Contact/Contact.css'
 import '../pages/uses/uses.css'
+import '../components/RegistrationForm/RegistrationForm.css'
 
 import React from 'react'
 import Head from 'next/head'
@@ -18,37 +19,37 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   <>
 
-  <Head>
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="https://unpkg.com/react-custom-scroll@4.3.0/dist/customScroll.css" rel="stylesheet" />
+    <Head>
+      <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+      <link href="https://unpkg.com/react-custom-scroll@4.3.0/dist/customScroll.css" rel="stylesheet" />
 
 
-    <title>Symbi Tech</title>
+      <title>Symbi Tech</title>
 
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-    <link href="//db.onlinewebfonts.com/c/61486c71563f7840de6d1c7e0a2ed07e?family=Cosi+Azure+Bold" rel="stylesheet" type="text/css"/>
+      <link href="//db.onlinewebfonts.com/c/61486c71563f7840de6d1c7e0a2ed07e?family=Cosi+Azure+Bold" rel="stylesheet" type="text/css" />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
 
-    <link href="https://fonts.googleapis.com/css2?family=Circular Std Bold:wght@100;200;300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet" /> 
+      <link href="https://fonts.googleapis.com/css2?family=Circular Std Bold:wght@100;200;300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 
-    <meta property="og:title" content="" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="" />
-    <meta property="og:image" content="" />
-    <meta property="og:description" content="" />
-  </Head>
+      <meta property="og:title" content="" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
+      <meta property="og:description" content="" />
+    </Head>
 
-  <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></Script>
+    <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></Script>
 
-  <Script src="https://unpkg.com/scrollreveal"></Script>
+    <Script src="https://unpkg.com/scrollreveal"></Script>
 
-  <Script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></Script>
+    <Script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></Script>
 
-  <Script src="https://unpkg.com/react-custom-scroll@4.3.0/dist/reactCustomScroll.js"></Script>
+    <Script src="https://unpkg.com/react-custom-scroll@4.3.0/dist/reactCustomScroll.js"></Script>
 
-  <Script src="https://unpkg.com/boxicons@2.0.9/dist/boxicons.js"></Script>
+    <Script src="https://unpkg.com/boxicons@2.0.9/dist/boxicons.js"></Script>
 
   </>
 
