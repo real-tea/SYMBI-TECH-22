eae712756323a243bb00b12fbb8cda54f3bcfcaa
Siddharth Khatri <siddxharth@gmail.com>
2022-09-08 10:52:49 +0530

    Change heading

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -11,13 +11,13 @@ export default function Landing() {
     return (
         <div className="landing" id="home">
             <Container>
-                        <Fade duration={2500} delay={500} big>
-                            {/* <h3 className="landing-h3">Hello, I&apos;m </h3> */}
-                            <h3 className="landing-h3">Welcome to </h3>
-                        </Fade>
-                        <Fade duration={2500} delay={1000}>
-                            <h1 className="gradient-text landing-h1">SymbITech</h1>
-                        </Fade>
+                <Fade duration={2500} delay={500} big>
+                    {/* <h3 className="landing-h3">Hello, I&apos;m </h3> */}
+                    <h3 className="landing-h3">Welcome to </h3>
+                </Fade>
+                <Fade duration={2500} delay={1000}>
+                    <h1 className="gradient-text landing-h1">SymbITech'22</h1>
+                </Fade>
             </Container>
         </div>
     )
