f93d54a2eebd2a700acdeff8dd8c4bf456ea3462
Siddharth Khatri <siddxharth@gmail.com>
2022-09-14 16:54:13 +0530

    Fix animation on Landing

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -9,17 +9,17 @@ export default function Landing() {
     return (
         <div className="landing" id="home">
             <Container>
-                        <Fade duration={2500} delay={500} big>
-                            {/* <h3 className="landing-h3">Hello, I&apos;m </h3> */}
-                            <h3 className="landing-h3">Welcome to </h3>
-                        </Fade>
-                        <Fade duration={2500} delay={1000}>
-                            <h1 className="gradient-text landing-h1">SymbITech&apos;22</h1>
-                        </Fade>
+                <Fade duration={2500} delay={500} big>
+                    {/* <h3 className="landing-h3">Hello, I&apos;m </h3> */}
+                    <h3 className="landing-h3">Welcome to </h3>
+                </Fade>
+                <Fade duration={2500} delay={1000}>
+                    <h1 className="gradient-text landing-h1">SymbITech&apos;22</h1>
+                </Fade>
 
-                        <Fade>
-                            <h1 className="landing-h3">28th-29th September</h1>
-                        </Fade>
+                <Fade duration={2500} delay={1000}>
+                    <h1 className="landing-h3">28th-29th September</h1>
+                </Fade>
             </Container>
         </div>
     )
