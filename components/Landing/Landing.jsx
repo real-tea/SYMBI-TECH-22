17bb5e3719bb27311f937bddb8075e5d98138e0f
real-tea <akash.0711a@gmail.com>
2022-09-14 16:27:56 +0530

    date for techfest

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -16,6 +16,10 @@ export default function Landing() {
                         <Fade duration={2500} delay={1000}>
                             <h1 className="gradient-text landing-h1">SymbITech&apos;22</h1>
                         </Fade>
+
+                        <Fade>
+                            <h1 className="landing-h3">28th-29th September</h1>
+                        </Fade>
             </Container>
         </div>
     )
