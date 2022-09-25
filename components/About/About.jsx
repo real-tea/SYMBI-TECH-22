eab0d2f9ce8c0bcf1d99e041d870e1c67d61f218
Siddharth Khatri <siddxharth@gmail.com>
2022-09-25 14:47:12 +0530

    Fix mobile view on About and Events section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import React from 'react'
 import Fade from 'react-reveal/Fade';
 
-import { Row, Col, Grid, Button, Spacer , Link } from '@nextui-org/react';
+import { Row, Col, Grid, Button, Spacer, Link } from '@nextui-org/react';
 
 // import { SiJavascript } from 'react-icons/si'
 
@@ -13,7 +13,7 @@ import Collage from '../../public/images/collage_about.png';
 
 export default function About() {
 
-    
+
     return (
         <div className="about" id="about">
             <h1 className="about-header">About</h1>
@@ -23,26 +23,26 @@ export default function About() {
                         <Fade bottom duration={2000}>
                             <p className="about-main-text">All Work, All Play</p>
                             {/* <p className="about-subtext">I&apos;m a Computer Science Undergraduate and a Frontend Developer from India 🇮🇳 Student at heart, and a Developer by profession, I make frontends look good with JavaScript and a sprinkle of CSS ✨ </p> */}
-                            <p className = "about-subtext-main">
-                                <span shadow color = "gradient" className="gradient-text">Symbiosis Institute of Technology </span>
-                            brings back its very own and well-renowned technical festival with added zest ✨ <br/> and enthusiasm this year. 
-For the first time, it is planned to be a centralized national-level event 🚀.<br/>
- {/* Everyone regardless of their branch will come together to showcase the perfect two days.  */}
+                            <p className="about-subtext-main">
+                                <span shadow color="gradient" className="gradient-text">Symbiosis Institute of Technology </span>
+                                brings back its very own and well-renowned technical festival with added zest ✨ <br /> and enthusiasm this year.
+                                For the first time, it is planned to be a centralized national-level event 🚀.<br />
+                                {/* Everyone regardless of their branch will come together to showcase the perfect two days.  */}
 
-<span shadow color = "gradient" className="gradient-text">SymbITech</span> will help young minds, broaden their vision, and surrender to their instincts for knowledge and <span shadow color="gradient" className="gradient-text">COSMOS</span> 👨‍🚀
-</p>    <br/>
-<p className="about-subtext">SymbITech, the Annual Technical Fest of SIT is all set to blow up everyone’s mind with lots off challenging events and knowledge hunts. 🤯</p>
+                                <span shadow color="gradient" className="gradient-text">SymbITech</span> will help young minds, broaden their vision, and surrender to their instincts for knowledge and <span shadow color="gradient" className="gradient-text">COSMOS</span> 👨‍🚀
+                            </p>    <br />
+                            <p className="about-subtext">SymbITech, the Annual Technical Fest of SIT is all set to blow up everyone’s mind with lots off challenging events and knowledge hunts. 🤯</p>
                             <p className="about-subtext"></p>
                             <Spacer y={1} />
-                            <Button  className="about-btn" size="xlarge" shadow color="gradient" auto>
-                            <Link href = "/about">Know More</Link>
+                            <Button className="about-btn" size="xlarge" shadow color="gradient" auto>
+                                <Link href="/about">Know More</Link>
                             </Button>
                         </Fade>
 
                     </Grid>
-                    <Grid xs={12} md={6} justify = "center" className="about-img-col">
+                    <Grid xs={12} md={6} justify="center" className="about-img-col">
                         <Fade bottom duration={2000}>
-                            <Image className="about-img" src={Collage} bottom="400px"/>
+                            <Image className="about-img" src={Collage} bottom="400px" />
                         </Fade>
                     </Grid>
                 </Grid.Container>
