8addafcba87a2aac2afbc028d9e153b172943d20
Siddharth Khatri <siddxharth@gmail.com>
2022-10-12 01:06:29 +0530

    Add sponsor to landing section

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -13,11 +13,12 @@ export default function Landing() {
                     {/* <h3 className="landing-h3">Hello, I&apos;m </h3> */}
                     <h3 className="landing-h3">Welcome to </h3>
                 </Fade>
-                <Fade duration={2500} delay={1000}>
+                <Fade duration={2500} delay={1500}>
+                    <h1 className='bajaj'>Bajaj Finserv’s</h1>
                     <h1 className="gradient-text landing-h1" shadow color="gradient">SymbITech&apos;22</h1>
                 </Fade>
 
-                <Fade duration={2500} delay={1000}>
+                <Fade duration={2500} delay={2500}>
                     <h1 className="landing-h3">11th-12th October</h1>
                 </Fade>
             </Container>
